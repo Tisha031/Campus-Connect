a4a3bff46d455cbfac294e0ec9dd36a997b5499d
Tisha031 <tishajinger0310@gmail.com>
2024-12-23 20:01:28 +0530

    Add files via upload

---
+++ database/for-SQLite/mysql.sql
@@ -0,0 +1,15 @@
+--CREATE TABLE emp1(empid INTEGER NOT NULL PRIMARY KEY, empname TEXT NOT NULL, email NOT NULL);--!
+
+
+
+--INSERT INTO emp(empid, empname, email) VALUES (1, 'Tish', 'test1@test.com');--!
+
+
+SELECT * FROM emp;
+
+
+
+--NSc--!
+
+
+
